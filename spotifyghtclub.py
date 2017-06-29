757fdb4da5e9fc87f8a94eff80b362649886c1f9
David Owen <david.owen.14@ucl.ac.uk>
2017-06-29 14:09:05 +0100

    Add an example track to an example playlist.

--- spotifyghtclub.py
+++ spotifyghtclub.py
@@ -1,17 +1,17 @@
 import credentials
 from spotipy.oauth2 import SpotifyClientCredentials
 import spotipy
+import spotipy.util
 import json
 import os.path
 
 def get_playlist_tracks(username, playlist_id):
-	results = sp.user_playlist_tracks(username,playlist_id)
+	results = sp.user_playlist_tracks(username, playlist_id)
 	tracks = results['items']
 	while results['next']:
 		results = sp.next(results)
 		tracks.extend(results['items'])
 	return tracks
-
 
 client_credentials_manager = SpotifyClientCredentials(client_id=credentials.client_id, client_secret=credentials.client_secret)
 sp = spotipy.Spotify(client_credentials_manager=client_credentials_manager)
@@ -34,6 +34,18 @@ else:
 		json.dump(tracks, outfile)
 
 for t in tracks:
-	print(t["track"]["name"])
+	#print(t["track"]["name"])
+	pass
 
-#print(json.dumps(tracks[0]["track"]["name"], indent=4))
+
+my_uri = 'spotify:user:ransaccc:playlist:5qcEczOgABfkROHMqbGfUX'
+username = my_uri.split(':')[2]
+playlist_id = my_uri.split(':')[4]
+track_ids = ['03p52fO3w4RrRnnEqAAM0w']
+
+scope = 'playlist-modify-public'
+token = spotipy.util.prompt_for_user_token(username, scope, client_id=credentials.client_id, client_secret=credentials.client_secret, redirect_uri='http://localhost:8888/callback')
+
+sp = spotipy.Spotify(auth=token)
+results = sp.user_playlist_add_tracks(username, playlist_id, track_ids)
+print(results)
